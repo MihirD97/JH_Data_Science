6e8d06558931f5bd2d82dc8a8644b48dc5b633d0
Mihir Deshpande <mihird97@gmail.com>
2021-12-06 21:44:25 +0530

    Update 1

--- C2W1_quiz.R
+++ C2W1_quiz.R
@@ -1,1 +1,12 @@
-data <- read.csv("quiz1_data/hw1_data.csv")
+data <- read.csv("quiz1_data/hw1_data.csv", header=TRUE)
+show(data)
+data[1:2,]
+summary(data)
+nrow(data)
+tail(data,2)
+data[47,]
+sum(is.na(data$Ozone))
+mean(data[!is.na(data$Ozone),]$Ozone)
+mean(data[!is.na(data$Ozone)&data$Ozone>31&data$Temp>90,]$Solar.R)
+mean(data[data$Month==6,]$Temp)
+max(data[!is.na(data$Ozone)&data$Month==5,]$Ozone)
